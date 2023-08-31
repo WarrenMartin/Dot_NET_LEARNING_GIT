c02dcefdeb8dcd3a21211fa07674d13d28136b2d
WarrenMartin <martinwarren2000@gmail.com>
2023-08-31 09:29:04 +0530

    thirtyfirstaug

--- Day2/Inheritance Example/Program.cs
+++ Day2/Inheritance Example/Program.cs
@@ -4,6 +4,18 @@
     {
         static void Main(string[] args)
         {
+            BaseClass o=new BaseClass();
+            o.PUBLIC=6;
+            o.PROTECTED_INTERNAL=9;
+            o.INTERNAL=10;
+
+            // the not allowed once are
+           // o.PRIVATE; Same class accessible 
+            // o.PROTECTED; accessible in derived class
+           // o.PRIVATE_PROTECTED; accessible in same class and derived class 
+
+
+            DerivedCLass o2=new DerivedCLass();
             
         }
     }
@@ -12,10 +24,16 @@
 
 public class BaseClass
 {
-    public int i;
+    public int PUBLIC;
+    private int PRIVATE;
+    private int PROTECTED;
+    internal int INTERNAL;
+    protected internal int PROTECTED_INTERNAL;
+    private protected int PRIVATE_PROTECTED;
+
 }
 
 public class DerivedCLass : BaseClass
-{ 
-    
+{
+    public void DoSomething() { }
 }
---
+++ Day2/TestAccessSpecifiers/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace TestAccessSpecifiers
+{
+
+    public class BaseClass
+    {
+        public int PUBLIC;
+        private int PRIVATE;
+        private int PROTECTED;
+        internal int INTERNAL;
+        protected internal int PROTECTED_INTERNAL;
+        private protected int PRIVATE_PROTECTED;
+
+    }
+
+}
