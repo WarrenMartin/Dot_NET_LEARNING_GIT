5cc3285d9eb8a6ab28d8aa1b1ecd8742c20d2872
WarrenMartin <martinwarren2000@gmail.com>
2023-09-04 09:05:41 +0530

    Day3

--- Day 3/Interfaces/Program.cs
+++ Day 3/Interfaces/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Method 1
+            Class1 o = new Class1();
+          //  o.Display();
+            //o.Insert();
+           // o.Update();
+            //o.Delete();
+
+            // Method 2
+            IDbFunctions oIDb;
+            oIDb = o;
+            o.Insert();
+
+            //Method 3
+           // ((IDbFunctions)o).Insert();
+
+
+
         }
     }
+
+    public interface IDbFunctions
+    {
+        void Insert();
+        void Update();
+        void Delete();
+    }
+
+    public class Class1 : IDbFunctions
+    {
+        public void Display()
+        {
+            Console.WriteLine("display from class1");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("delete from class1");
+        }
+
+        public void Insert()
+        {
+            Console.WriteLine("insert from class1");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("update from class1");
+        }
+    }
+
 }
