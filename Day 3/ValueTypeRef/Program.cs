05f7ba03cf8c375d2e0b07ba3a9fced483945f8d
WarrenMartin <martinwarren2000@gmail.com>
2023-09-08 07:31:56 +0530

    third day third

---
+++ Day 3/ValueTypeRef/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ValueTypeRef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+            int j = 200;
+           // Swap(ref i, ref j);
+           Init(out i,out j);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
+        }
+
+        static void Swap(ref int a,ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Init(out int a, out int b) 
+        {
+            a = 1000;
+            b = 2000;
+        }
+
+    }
+
+    
+}
