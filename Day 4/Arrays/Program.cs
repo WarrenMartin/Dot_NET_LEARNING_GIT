0f0d0070b49b42d5d1369c1bc2099e3fb2d77f9f
WarrenMartin <martinwarren2000@gmail.com>
2023-09-09 10:12:52 +0530

    arrays

---
+++ Day 4/Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr=new int[3];
+
+          
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //To Read Values from the users
+                //Console.WriteLine("enter Value for arr[" +i + "]:"); //Method is called String concatenation
+                //Console.WriteLine("enter value for arr[{0}]:",i); //placeholders
+                //Console.WriteLine($"Enter value for arr[{i}]:") //String interpolation
+
+                //arr[i] = int.Parse(Console.ReadLine());
+                arr[i] =Convert.ToInt32(Console.ReadLine());
+
+            }
+            foreach (int item in arr)
+            { 
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Day 4/NullableTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace NullableTypes
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int? i;
+            i = null;
+            //int j = (int)i;
+            j=i.GetValueOrDefault();
+
+            // or you can do it like this also
+            i = i ?? 0; // ?? = null coalescing operator 
+
+        }
+    }
+}
