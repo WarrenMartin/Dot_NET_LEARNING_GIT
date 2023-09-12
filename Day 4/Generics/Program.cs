0b7a2742be9390a3d008f8bb579017113d986b42
WarrenMartin <martinwarren2000@gmail.com>
2023-09-12 07:09:53 +0530

    Generics

---
+++ Day 4/Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            MyClass<int> myClass = new MyClass<int>();
+            MyClass<String> myString = new MyClass<String>();
+
+            int n1 = 10;
+            int n2 = 20;
+            Console.WriteLine($"{n1} {n2}");
+
+            myClass.Swap(ref n1, ref n2);
+            Console.WriteLine($"{n1}  {n2}");
+        }
+    }
+
+
+     public class MyClass<T>
+    {
+        public void Swap(ref T i, ref T j)
+        {
+            T temp = i;
+            i = j;
+            j = temp;
+        }
+    }
+
+}
