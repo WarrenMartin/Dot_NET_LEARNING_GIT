ff555260c8e07c51393807e81aa786c0e1149ce5
WarrenMartin <martinwarren2000@gmail.com>
2023-09-12 09:33:24 +0530

    Collections

---
+++ Day 4/Collections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList objArrayList = new ArrayList();
+            objArrayList.Add(10);
+            objArrayList.Add("vikram");
+            objArrayList.Add(true);
+            objArrayList.Add(4.234);
+
+            objArrayList.Remove("vikram"); // remove vikram out from here
+            objArrayList.RemoveAt(0); // removeat removes the 0th element 
+
+            Console.WriteLine(objArrayList.Count);
+
+            foreach (object i in objArrayList) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
