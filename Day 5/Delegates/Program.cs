c96352d7e07472c2b60a763c8c431bd72f43630a
WarrenMartin <martinwarren2000@gmail.com>
2023-09-17 16:19:30 +0530

    Day 5

---
+++ Day 5/Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Delegates
+
+
+{
+    //Step 1: Create a delegate class having the same signature as the function to call 
+    public delegate void Disp1();
+    internal class Program
+    {
+        static void Main()
+        {
+            //step 2 : declare a delegate reference. and a delegate object. 
+            // the delegate object has the function name as a parameter.
+
+            Disp1 dsp1=new Disp1(Display);
+            dsp1();
+
+            Display();
+        }
+
+        static void Display() 
+        {
+            Console.WriteLine("Display called");
+        }
+    }
+}
