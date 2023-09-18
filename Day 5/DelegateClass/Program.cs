8aa5fa73e7660ea6b12a2346df8ee83832c8f9ff
WarrenMartin <martinwarren2000@gmail.com>
2023-09-18 10:58:10 +0530

    do

---
+++ Day 5/DelegateClass/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DelegateClass
+
+    /*
+     * Remeber the heriachy 
+     * Object
+     * Delegate
+     * MulticastDelegate
+     * Del1 
+     */
+{
+    public delegate void Del1();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is an Equivanent of +=
+           Delegate objDel= Delegate.Combine(new Del1(Display), new Del1(Show)); 
+           //Delegate.remove // This is the Equvalent of ==
+
+
+        }
+
+        static void Display() {
+            Console.WriteLine("Hello Display");
+        }
+
+        static void Show() { 
+            Console.WriteLine("Hello Show");
+        }
+
+        static int Add(int a, int b) 
+        { 
+            return a + b;
+        }
+        static int Subtract(int a, int b)
+        { return a - b; }
+    }
+}
