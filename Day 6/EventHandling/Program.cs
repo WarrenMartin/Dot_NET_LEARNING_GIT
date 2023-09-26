c66a654b4abf1762e217378a0f873a8d81738a84
WarrenMartin <martinwarren2000@gmail.com>
2023-09-26 20:14:56 +0530

    EventHandling

---
+++ Day 6/EventHandling/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace EventHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    // Step 1: Create a delegate class having the same signature as the event handling function
+
+
+    public delegate void InvalidP1EventHandler();
+
+    
+
+    public class Class1
+    {
+        // Step 2: declare the event of the delegate type
+        // event is a delegate reference
+
+        public event InvalidP1EventHandler InvalidP1;
+
+        private int p1;
+        public int P1
+        {
+            get 
+            {
+                return p1;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    p1 = value;
+                }
+                else 
+                {
+                    //Raising an Event
+                    //step 3-raise the event whenever required
+                    //to raise the event call the delegate reference
+
+                    if (InvalidP1 != null)
+                    {
+                        InvalidP1();
+                    }
+                    // Now we need to put an additional check  saying if INvalidP1() not eqaul to null then call it else you will get an null pointer exception
+                }
+            }
+        }
+    }
+}
